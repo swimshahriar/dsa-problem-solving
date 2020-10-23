b2305fd11b963d664d0fcc194cfac59feb5587a6
Shahriar Swim <shahriar2k18@gmail.com>
2020-10-23 20:45:27 +0600

    day 6,7 problems solutions

---
+++ JavaScript/HackerRank/10-days-of-js/day-6-dateStringToWeekDay.js
@@ -0,0 +1,17 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+  let dayName;
+  // Write your code here
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  dayName = dayNames[new Date(dateString).getDay()];
+
+  return dayName;
+}
