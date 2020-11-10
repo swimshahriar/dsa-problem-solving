a9fe0ff46e3ac9fedd1f92a6b0dc1bf1532d8fe7
Shahriar Swim <shahriar2k18@gmail.com>
2020-11-10 21:17:27 +0600

    python list challenge hackerrank

---
+++ Python/HackerRank/python/lists.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    N = int(input(""))
+
+    list = []
+    user_input_list = []
+
+    for _ in range(N):
+        query = input().split()
+
+        if query[0] == "print":
+            print(list)
+        elif query[0] == "insert":
+            list.insert(int(query[1]), int(query[2]))
+        elif query[0] == "remove":
+            list.remove(int(query[1]))
+        elif query[0] == "append":
+            list.append(int(query[1]))
+        elif query[0] == "sort":
+            list.sort()
+        elif query[0] == "pop":
+            list.pop()
+        elif query[0] == "reverse":
+            list.reverse()
