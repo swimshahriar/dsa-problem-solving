1fc3dcd877b76630f40da00a5b531609dd157683
Shahriar Swim <shahriar2k18@gmail.com>
2022-01-13 13:20:38 +0600

    search suggestions

--- searchAlgo.js
+++ searchAlgo.js
@@ -1,21 +1,18 @@
+console.clear();
 class Search {
   #dictionary = {};
+  #suggestionsLimit = 5;
 
   insert(word) {
     let currDictionary = this.#dictionary;
 
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (currDictionary['next'] === undefined) {
-        currDictionary['next'] = {};
-        currDictionary['next'][char] = {};
-      } else {
-        if (currDictionary['next'][char] === undefined) {
-          currDictionary['next'][char] = {};
-        }
+    for (let char of word) {
+      if (currDictionary[char] === undefined) {
+        currDictionary[char] = { next: { wordList: new Set() } };
       }
-
-      currDictionary = currDictionary['next'][char];
+      const wordList = currDictionary[char]['next']['wordList'];
+      wordList?.size < #suggestionsLimit && wordList.add(word);
+      currDictionary = currDictionary[char]['next'];
     }
 
     currDictionary['done'] = true;
@@ -24,39 +21,59 @@ class Search {
   find(word) {
     let currDictionary = this.#dictionary;
 
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (!currDictionary['next'] || !currDictionary['next'][char]) {
+    for (let char of word) {
+      if (currDictionary[char] === undefined) {
         return false;
       } else {
-        currDictionary = currDictionary['next'][char];
+        currDictionary = currDictionary[char]['next'];
       }
     }
 
     return currDictionary['done'] || false;
   }
 
+  search(queryString) {
+    let currDictionary = this.#dictionary;
+    let suggestions = {};
+
+    for (let char of queryString) {
+      if (currDictionary[char] !== undefined) {
+        currDictionary = currDictionary[char]['next'];
+        suggestions = currDictionary['wordList'];
+      }
+    }
+
+    return suggestions;
+  }
+
   delete(word) {
     let currDictionary = this.#dictionary;
 
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (currDictionary['next'][char] !== undefined) {
-        currDictionary = currDictionary['next'][char];
-      } else return console.log('Word is not in dictionary.');
+    for (let char of word) {
+      if (currDictionary[char]['next'] !== undefined) {
+        currDictionary = currDictionary[char]['next'];
+        currDictionary['wordList']?.delete(word);
+      } else {
+        return console.log('Word is not in dictionary.');
+      }
     }
     delete currDictionary['done'];
   }
 
   get data() {
-    return JSON.stringify(this.#dictionary, null, 2);
+    return this.#dictionary;
+  }
+
+  set suggestionWordLimit(number) {
+    this.#suggestionsLimit = number;
   }
 }
 
 const s = new Search();
-s.insert('hello');
+s.insert('hi');
 s.insert('hie');
-s.insert('bi');
-s.delete('bi');
+s.insert('hei');
+// s.delete('hi');
 // console.log(s.find('hie'));
-console.log(s.data);
+console.log(s.search('hi'));
+// console.log(s.data);
